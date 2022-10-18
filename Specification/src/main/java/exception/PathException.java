9e2a33d1feface2f57dabe223118713e6645000f
Luka <lpavlovic11521rn@raf.rs>
2022-10-18 03:12:05 +0200

    added PathException and StorageException

---
+++ Specification/src/main/java/exception/PathException.java
@@ -0,0 +1,8 @@
+package exception;
+
+public class PathException extends Exception{
+	
+	public PathException(String msg) {
+		super(msg);
+	}
+}
---
+++ Specification/src/main/java/exception/StorageException.java
@@ -0,0 +1,9 @@
+package exception;
+
+@SuppressWarnings("serial")
+public class StorageException extends Exception{
+	
+	public StorageException(String msg) {
+		super(msg);
+	}
+}
